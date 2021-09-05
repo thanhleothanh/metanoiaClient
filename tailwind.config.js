4e1558213ad0ab84b12427f16a29867720ed050a
thanhleothanh <thanhng260588@gmail.com>
2021-09-05 18:44:19 +0700

    small changes after feedback

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -42,6 +41,7 @@ module.exports = {
       ...defaultTheme.screens,
       '2xl': '1700px',
       '3xl': '2000px',
+      standalone: { raw: '(display-mode: standalone)' },
     },
   },
   variants: {
